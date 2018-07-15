14c7e868d49d8a2a49337ffdd3b6ba09dbc16c87
shubh2565 <shubh2565@users.noreply.github.com>
2018-07-15 14:13:28 +0530

    Add files via upload

---
+++ plot4.R
@@ -0,0 +1,21 @@
+setwd('/home/shubham/data_science/exploratory_data_analysis')
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+combustionRelated <- grepl("comb", SCC$SCC.Level.One, ignore.case=TRUE)
+coalRelated <- grepl("coal", SCC$SCC.Level.Four, ignore.case=TRUE) 
+coalCombustion <- (combustionRelated & coalRelated)
+combustionSCC <- SCC[coalCombustion,]$SCC
+combustionNEI <- NEI[NEI$SCC %in% combustionSCC,]
+
+
+library(ggplot2)
+
+png("plot4.png", width=720, height=480)
+ggp <- ggplot(combustionNEI,aes(factor(year),Emissions/10^5)) +
+  geom_bar(stat="identity",fill="grey",width=0.75) +
+  theme_bw() +  guides(fill=FALSE) +
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Coal Combustion Source Emissions Across US from 1999-2008"))
+print(ggp)
+dev.off()
